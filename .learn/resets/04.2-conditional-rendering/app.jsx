3d38eca1537eea295c16bcaecee9e8cea2f85af1
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-02-07 19:12:42 +0000

    exercises react done

--- .learn/resets/04.2-conditional-rendering/app.jsx
+++ .learn/resets/04.2-conditional-rendering/app.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
+// Add the green inside this function
 const Alert = props => {
-	// Your component here
-	if (props.color == 'red') {
-		return (
-			<div className="alert alert-danger" role="alert">
-				{props.text}
-			</div>
-		)
-	} else if (props.color == 'yellow') {
-		return (
-			<div className="alert alert-warning" role="alert">
-				{props.text}
-			</div>
-		)
-	}
+	const colorClasses = {
+		red: "alert-danger",
+		yellow: "alert-warning"
+	};
+	
+	if (colorClasses[props.color] === undefined) alert(`The color ${props.color} is not in the possible list of colors`);
+
+	return (
+		<div className={`alert ${colorClasses[props.color]}`} role="alert">
+			{props.text}
+		</div>
+	);
 };
 Alert.propTypes = {
 	color: PropTypes.string,
@@ -24,11 +23,12 @@ Alert.propTypes = {
 };
 
 // Here is where the <Alert /> component is being used, you don't have to edit this part
-// But it helps you understand what properties are being passed to the component
+// But it helps you understand what properties is the component using
 ReactDOM.render(
 	<div>
 		<Alert text="OMG! Something really bad has happened!" color="red" />
 		<Alert text="Well, it is not that bad after all!" color="yellow" />
+		<Alert text="I am supposed to be green" color="green" />
 	</div>,
 	document.querySelector("#myDiv")
 );
